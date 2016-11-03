35293a1ba92de1cab16c540906ee7430885c4010
Thorpe, Geoff <Geoff.Thorpe@ft.com>
2016-11-03 13:49:15 +0000

    applying origami formatting

--- public/js/forms.js
+++ public/js/forms.js
@@ -5,62 +5,69 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
 	/**
-	 * Check the validity of a field on page load and each time the field changes
+	 * Check the validity of a field on page load and each time the field changes 
 	 **/
 	function addChecks(input, group) {
 		function checkValidity() {
-			var classes = group.getAttribute("class") == null ? "" : group.getAttribute("class");
 			if(!input.value) {
-				group.setAttribute("class", classes);
+				group.setAttribute("class","o-forms-group");
 			} else if(input.checkValidity()){
-				group.setAttribute("class", classes + " o-forms--valid");
+				group.setAttribute("class","o-forms-group o-forms--valid");
 			} else {
-				group.setAttribute("class", classes + " o-forms--error");
+				group.setAttribute("class","o-forms-group o-forms--error");
 			}
 		}
 		input.addEventListener('change', checkValidity);
 		checkValidity();
 	}
+	var systemcodeinput = document.querySelector('#systemCode');
+	addChecks(systemcodeinput, systemcodeinput.parentNode);
 
 	var idinput = document.querySelector('#id');
-	addChecks(idinput, idinput.parentNode);
+	// checks for duplicate IDs are done prior to this function, no need for AddChecks call
 
-	var nameinput = document.querySelector('#name');
-	addChecks(nameinput, nameinput.parentNode);
+	var baseinput = document.querySelector('#base');
+	addChecks(baseinput, baseinput.parentNode);
 
-	var descinput = document.querySelector('#description');
-	addChecks(descinput, descinput.parentNode);
+	var protocolinput = document.querySelector('#protocol');
 
-	var tierinput = document.querySelector('#serviceTier');
-	addChecks(tierinput, tierinput.parentNode);
+	function updateBaseURL() {
+		var baseurl = (protocolinput.value == "both") ? "http" : protocolinput.value;
+		baseurl += "://" + baseinput.value + "/";
+		var baseurlnodes = document.querySelectorAll(".baseurl"), i;
+		for (i = 0; i < baseurlnodes.length; i++) {
+			baseurlnodes[i].firstChild.nodeValue = baseurl;
+		}
+	}
+	idinput.addEventListener('change', updateBaseURL);
+	protocolinput.addEventListener('change', updateBaseURL);
+	baseinput.addEventListener('change', updateBaseURL);
 
-	var stageinput = document.querySelector('#lifecycleStage');
-	addChecks(stageinput, stageinput.parentNode);
-
-	var primaryinput = document.querySelector('#primary_name');
-	addChecks(primaryinput, primaryinput.parentNode);
-
-	var secondaryinput = document.querySelector('#secondary_name');
-	addChecks(secondaryinput, secondaryinput.parentNode);
-
-	var programmeinput = document.querySelector('#prog_name');
-	addChecks(programmeinput, programmeinput.parentNode);
-
-	var productownerinput = document.querySelector('#prod_name');
-	addChecks(productownerinput, productownerinput.parentNode);
-
-	var techleadinput = document.querySelector('#tech_name');
-	addChecks(techleadinput, techleadinput.parentNode);
-
-	var architecvtureinput = document.querySelector('#architectureDiagram');
-	addChecks(architectureinput, architectureinput.parentNode);
-
-	var repoinput = document.querySelector('#gitRepository');
-	addChecks(repoinput, repoinput.parentNode);
-
-	var hostinput = document.querySelector('#hostPlatform');
-	addChecks(hostinput, hostinput.parentNode);
-
-	var moreinput = document.querySelector('#moreInformation');
-	addChecks(moreinput, moreinput.parentNode);
+	var urlnodes = document.querySelectorAll(".url"), i;
+	for (i = 0; i < urlnodes.length; i++) {
+		checkValidity(urlnodes[i]);
+	}
+	function checkValidity(urlnode) {
+		fetch(urlnode.dataset.validateapi, {headers:{
+			key: urlnode.dataset.apikey,
+		}}).then(function (response) {
+			return response.json();
+		}).then(function (body) {
+			var msg, classname;
+			console.log(body);
+			if (!body.isValid) {
+				msg = "This is healthcheck is invalid";
+				classname = "invalid";
+			} else if(body.needsUpgrade) {
+				msg = "This healthcheck is valid, but isn't up-to-date with the latest standard";
+				classname = "upgrade";
+			} else {
+				msg = "This healthcheck is valid";
+				classname = "valid";
+			}
+			var savebutton = urlnode.querySelector(".save-button");
+			savebutton.firstChild.nodeValue = msg;
+			savebutton.className += " " + classname;
+		})
+	}
 });
