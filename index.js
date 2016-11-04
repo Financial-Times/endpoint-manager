98e5d1d3247c896648208441b64b9b5f647cc87d
Thorpe, Geoff <Geoff.Thorpe@ft.com>
2016-11-04 09:47:11 +0000

    declare querystring requirement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require("request");
 var bodyParser = require('body-parser');
+const querystring = require('querystring');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
