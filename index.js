6a9ab1b15af3b25c9b4362e02d9c8ef0affc3bf8
Thorpe, Geoff <Geoff.Thorpe@ft.com>
2016-12-09 13:01:40 +0000

    spelling

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
     res.setHeader('Cache-Control', 'no-cache');
     console.time('CMDB api call for all endpoints')
     sortby = req.query.sortby
-	cmdb._fetchAll(res.locals, endpointsurl(req)).then(function (endpoints) {
+	cmdb._fetchAll(res.locals, endpointsURL(req)).then(function (endpoints) {
 		endpoints.forEach(indexController);
 		endpoints.sort(CompareOnKey(sortby));
         console.timeEnd('CMDB api call for all endpoints')
