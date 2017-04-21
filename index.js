7e766320dc9a91d7113b62293725b579e4dfd4ec
geoff thorpe <geoff.thorpe@ft.com>
2017-04-21 10:59:36 +0100

    ensure isLive has boolean<->text translation

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ app.post('/manage/:endpointid', function (req, res) {
 		protocol: req.body.protocol,
 		healthSuffix: req.body.healthSuffix,
 		aboutSuffix: req.body.aboutSuffix,
-		isLive: !!req.body.isLive,
+		isLive: req.body.isLive,
 	}
 	if (req.body.systemCode) endpoint.isHealthcheckFor = {system: [{'dataItemID':req.body.systemCode}]};
 	cmdb.putItem(res.locals, 'endpoint', req.params.endpointid, endpoint).then(function (result) {
